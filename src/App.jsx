1e1b5863bd58521b21cc225e35a0b629d4b0fadf
eachavez1 <slipkanot2010@hotmail.com>
2024-05-24 18:49:33 -0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from './components/Nav/Nav'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EjemploContadorEventListener from './components/examples/EjemploContadorEventListener';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/PreEntrega1-Chaves" element={<ItemListContainer saludo="Bienvenido a la tienda en linea de Apple"/>} />
         <Route path="/category/:idCategory" element={<ItemListContainer saludo="Bienvenido a la tienda en linea de Apple"/>} />
         <Route path="/detail/:idProduct" element={<ItemDetailContainer/>}/>
+        <Route path="/example" element={<EjemploContadorEventListener/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/examples/EjemploContadorEventListener.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EjemploContadorEventListener = () => {
+  return (
+    <div>EjemploContadorEventListener</div>
+  )
+}
+
+export default EjemploContadorEventListener
