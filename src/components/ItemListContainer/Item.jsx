61f1b28dbd3e3c0538c680e3cd5d8eefecf95003
eachavez1 <slipkanot2010@hotmail.com>
2024-05-24 19:59:48 -0500

    eventos

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Item = ({product}) => {
+
+  const [expandir, setExpandir] = useState(false)
+
+  const handleMouseOver = () => {
+    setExpandir(true)
+  };
+
+  const handleMouseLeave = () => {
+    setExpandir(false)
+  };
+
+  const estiloCard = {
+
+    transform:expandir ? "scale(0.9)" : "scale(1)",
+    transition: "transform 0.4s ease-in-out"
+  }
+
+
   return (
-    <div key={product.id} className="card">
+    <div key={product.id} 
+    style={estiloCard}
+    className="card" 
+    onMouseOver={handleMouseOver} 
+    onMouseLeave={handleMouseLeave}>
         <div className="image-box-card">
             <img className="image-card" src={product.imagen} alt={product.nombre} />
         </div>
--- src/components/examples/EjemploContadorEventListener.jsx
+++ src/components/examples/EjemploContadorEventListener.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const EjemploContadorEventListener = () => {
+    const [contador, setContador] = useState(1)
+
+    const handleClick = () => {
+
+        setContador((prevContador) => prevContador + 1)
+    }
+
+    useEffect (()=>{
+
+        const botonSumar=document.getElementById("sumar")
+        botonSumar.addEventListener("click", handleClick)
+
+        return () => {
+            botonSumar.removeEventListener("click", handleClick)
+        }
+
+    },[])
+
   return (
-    <div>EjemploContadorEventListener</div>
+    <div>
+        <p>{contador}</p>
+        <button id="sumar">+</button>
+
+    </div>
   )
 }
 
